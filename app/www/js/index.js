3c3abe63e8bcff105365445dc531daa8ffaf0a19
Metod Medja <metod@medja.net>
2016-06-21 14:55:41 +0000

    add basic app functions

---
+++ app/www/js/index.js
@@ -0,0 +1,55 @@
+/* global google, fetch, navigator */
+
+var isCentered = false;
+var currentPosition;
+
+var gallery = document.getElementById('gallery');
+var cameraButton = document.getElementById('camera');
+var closeButton = document.getElementById('close');
+
+var map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 46.2427669, lng: 14.353608 },
+    zoom: 16,
+    disableDefaultUI: true
+});
+
+document.addEventListener('deviceready', function() {
+    navigator.geolocation.watchPosition(function(current) {
+        currentPosition = {
+            lat: current.coords.latitude,
+            lng: current.coords.longitude
+        };
+        
+        if (!isCentered) {
+            map.setCenter(currentPosition);
+            isCentered = true;
+        }
+    });
+});
+
+cameraButton.addEventListener('click', function() {
+    navigator.camera.getPicture(function(image) {
+        var url = 'data:image/jpeg;base64,' + image;
+        
+        addImage(url, currentPosition);
+    }, function () {}, {
+        destinationType: 0
+    });
+});
+
+function addImage(url, location) {
+    var marker = new google.maps.Marker({
+        animation: google.maps.Animation.DROP,
+        position: location,
+        map: map
+    });
+    
+    marker.addListener('click', function() {
+        gallery.style.backgroundImage = 'url(' + url + ')';
+        gallery.style.transform = 'translateY(0)';
+    });
+}
+
+closeButton.addEventListener('click', function() {
+    gallery.style.transform = 'translateY(100%)';
+});
